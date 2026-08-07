0cf730a873a1dee7f1632d933da8d69752817dce
elytva <yanratanaksambath@gmail.com>
2026-08-08 01:17:37 +0700

    refresh

--- configuration.nix
+++ configuration.nix
@@ -15,6 +15,12 @@
     options thinkpad_acpi fan_control=1
   '';
 
+  # Graphics
+  hardware.graphics = {
+  enable = true;
+  enable32Bit = true;
+  };
+
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Kernel
@@ -26,7 +32,6 @@
 
   # Time Zone
   time.timeZone = "Asia/Phnom_Penh";
-
   i18n.defaultLocale = "en_US.UTF-8";
 
   # Display Services
@@ -46,12 +51,12 @@
   enable = true;
   settings = {
     CPU_SCALING_GOVERNOR_ON_AC = "performance";
-    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave"; # Adjust to your needs
 
     CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-    CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+    CPU_ENERGY_PERF_POLICY_ON_BAT = "power;
     
-    START_CHARGE_THRESH_BAT0 = 40;
+    START_CHARGE_THRESH_BAT0 = 75;
     STOP_CHARGE_THRESH_BAT0 = 80;
     };
   };
@@ -61,12 +66,14 @@
      extraGroups = [ "wheel" "networkmanager" ];
      packages = with pkgs; [
        tree
+       wget
      ];
    };
 
    services.thermald.enable = true;
    services.throttled.enable = true;
-   
+
+  # Optional
   programs.gpu-screen-recorder = {
     package = inputs.gsr-ui-nix.packages.${pkgs.stdenv.hostPlatform.system}.gpu-screen-recorder;
     enable = true;
@@ -80,6 +87,7 @@
   services.openssh.enable = true;
   programs.ssh.startAgent = true;
 
+  # Do not edit
   system.stateVersion = "26.05";
 
 }
