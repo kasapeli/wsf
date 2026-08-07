67eeaf47e9b06ee27bf6bde4a2a010259a4d0730
elytva <yanratanaksambath@gmail.com>
2026-08-08 01:44:05 +0700

    add missing quote

--- configuration.nix
+++ configuration.nix
@@ -54,7 +54,7 @@
     CPU_SCALING_GOVERNOR_ON_BAT = "powersave"; # Adjust to your needs
 
     CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-    CPU_ENERGY_PERF_POLICY_ON_BAT = "power;
+    CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
     
     START_CHARGE_THRESH_BAT0 = 75;
     STOP_CHARGE_THRESH_BAT0 = 80;
